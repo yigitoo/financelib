a107a9805bab40da18746769405cf9f97ac7dce2
Yiğit GÜMÜŞ <gumusyigit101@gmail.com>
2024-12-22 18:41:52 +0300

    change __init__.py

--- src/financelib/__init__.py
+++ src/financelib/__init__.py
@@ -5,5 +5,6 @@ FinanceLib - Simple finance library for stock market data
 from financelib.stock import Stock
 
 __version__ = "0.1.0"
-__author__ = "Your Name"
+__author__ = "Yiğit GÜMÜŞ"
+__email__ = "gumusyigit101@gmail.com"
 __all__ = ["Stock"]
